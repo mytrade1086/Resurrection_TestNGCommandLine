5d296ed61fc35044897797c51590d2783dbb4bc3
Sumit <sumit@sumit.com>
2021-12-07 09:05:25 +0530

    New Tests

---
+++ Resurrection_TestNGCommandLine/src/test/java/Resurrection_TestNGCommandLine/Resurrection_TestNGCommandLine/Test1.java
@@ -0,0 +1,18 @@
+package Resurrection_TestNGCommandLine.Resurrection_TestNGCommandLine;
+
+
+
+// //mvn clean test -Dsurefire.suiteXmlFiles=/path/to/testng.xml
+public class Test1 {
+
+	@org.testng.annotations.Test(groups= {"Test1"})
+	public void Method1() {
+		
+	
+		
+		System.out.println("From Test1");
+		
+	}
+	
+	
+}
---
+++ Resurrection_TestNGCommandLine/src/test/java/Resurrection_TestNGCommandLine/Resurrection_TestNGCommandLine/Test2.java
@@ -0,0 +1,11 @@
+package Resurrection_TestNGCommandLine.Resurrection_TestNGCommandLine;
+
+public class Test2 {
+
+@org.testng.annotations.Test(groups= {"Test2"})
+public void MEthod2() {
+		System.out.println("From Test2");
+	}
+
+
+}
